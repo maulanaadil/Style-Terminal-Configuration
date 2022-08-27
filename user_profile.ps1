d013d4884d307999d106a102f8552726522dd3ba
Maulana Adil <59109741+maulanaadil@users.noreply.github.com>
2022-08-27 22:52:46 +0700

    Add files via upload

---
+++ user_profile.ps1
@@ -0,0 +1,71 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+$omp_config = Join-Path $PSScriptRoot ".\maulnad.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression 
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias y yarn
+Set-Alias n npm
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+ function which ($command) {
+     Get-Command -Name $command -ErrorAction SilentlyContinue |
+	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+ } 
+
+
+function touch {
+  Param(
+    [Parameter(Mandatory=$true)]
+    [string]$Path
+  )
+
+  if (Test-Path -LiteralPath $Path) {
+    (Get-Item -Path $Path).LastWriteTime = Get-Date
+  } else {
+    New-Item -Type File -Path $Path
+  }
+}
+
+function co
+{
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter({
+            param($pCmd, $pParam, $pWord, $pAst, $pFakes)
+
+            $branchList = (git branch --format='%(refname:short)')
+
+            if ([string]::IsNullOrWhiteSpace($pWord)) {
+                return $branchList;
+            }
+
+            $branchList | Select-String "$pWord"
+        })]
+        [string] $branch
+    )
+
+    git checkout $branch;
+}
